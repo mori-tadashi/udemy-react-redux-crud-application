b6f37cd55af3f99fed54eccfa9a4ef2fe3dc2596
mori-tadashi <tmori@milize.co.jp>
2021-03-02 13:51:45 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 function App() {
+    const greeting = "Hi, Tom";
+    const dom = <h1>{greeting}</h1>
   return (
-    <div>
-     <h1>
-       Hello world
-     </h1>
-    </div>
+      <div>
+          <label htmlFor="bar">bar</label>
+          <input type="text" onChange={() => {console.log("I am clicked!")}} />
+      </div>
   );
 }
 
